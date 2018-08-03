0d3000c07b6c5c9b4c8c5ec5172e2c0c694e1cc4
CAZET Quiterie [IT-CE] <quiterie.cazet@it-ce.fr>
2018-08-03 14:32:24 +0200

    first commit

---
+++ js/players.js
@@ -0,0 +1,17 @@
+function Player(name, hp, weapon) {
+    this.name = "",
+        this.hp = 100,
+        this.weapon = "hands"
+};
+
+
+
+var firstPlayer = new Player("Player One", 100, this.weapon)
+console.log(firstPlayer)
+
+
+function getPlayers() {
+
+    document.getElementById('player').innerHTML = firstPlayer;
+
+}
